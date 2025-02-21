7b925e0558cec9837db9d1180f236145eaab0350
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-21 15:51:26 +0530

    type

---
+++ typeof_datatype.py
@@ -0,0 +1,14 @@
+number=2541
+floating_number=52.214563
+character='a'
+stringstring="this is a string"
+boolean=True
+nonetype=None
+complexnumber=1+4j
+print(type(number))
+print(type(floating_number))
+print(type(character))
+print(type(stringstring))
+print(type(boolean))
+print(type(nonetype))
+print(type(complexnumber))
