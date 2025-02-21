96c375bde2525d8d72742abfa9b56275c8096d66
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-21 16:51:03 +0530

    initial commit

---
+++ immutable_tuple.py
@@ -0,0 +1,14 @@
+tuple=(4,5,8,"bittu",2.2145,True,None)
+print(tuple)
+print(type(tuple))
+print(tuple[0])
+# tuple[2]=100 cannot modify the tuple like string
+tup2=()
+tup3=(1,) # , is necessary to make this tuple
+tup4=(1) # this will act like integer
+print(tup2)
+print(tup3)
+print(tup4)
+print(type(tup2))
+print(type(tup3))
+print(type(tup4))
