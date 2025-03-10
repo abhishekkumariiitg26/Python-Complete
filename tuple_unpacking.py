a836238442e87d3f0f63575c64e55bdd78bee90f
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-03-10 09:43:26 +0530

    initial commit

---
+++ tuple_unpacking.py
@@ -0,0 +1,11 @@
+coordinates=(1,2,3)
+print(coordinates[0])
+print(coordinates[1])
+print(coordinates[2])
+x=coordinates[0]
+y=coordinates[1]
+z=coordinates[2]
+print(x,y,z)
+# This is unpacking
+a,b,c=coordinates
+print(a,b,c)
