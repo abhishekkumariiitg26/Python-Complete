f6e6be28a31c95cc034de6e3e06d4d99e2d6b365
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-03-28 12:52:15 +0530

    second commit

--- carriage_return_loading.py
+++ carriage_return_loading.py
@@ -1,6 +1,13 @@
-import time
+# import time
 
+# for i in range(101):
+#     print(f"\rLoading... {i}%", end="", flush=True)
+#     time.sleep(0.05)
+# print("\nDone!")
+
+
+import time
 for i in range(101):
-    print(f"\rLoading... {i}%", end="", flush=True)
+    print(f"\rLoading... {i}%",end="",flush=True)
     time.sleep(0.05)
 print("\nDone!")
